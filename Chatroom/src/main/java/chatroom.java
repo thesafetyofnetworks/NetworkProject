fbd3b21897ed9ce2343e84707a8328930486289d
FIroY <34035050+FIroY@users.noreply.github.com>
2019-04-27 15:05:31 +0800

    UI 1.0
    This is the first version of UI, including sign in, register, and chatroom.

---
+++ Chatroom/src/main/java/chatroom.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class chatroom {
+    public JPanel panel1;
+    private JList list1;
+    private JTextArea textArea3;
+    private JButton exitButton;
+    private JButton privateMessageButton;
+    private JButton sendButton;
+    private JTextArea textArea1;
+    private JTextArea textArea2;
+
+    public chatroom() {
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            //点击发送之后的操作
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+    }
+}
